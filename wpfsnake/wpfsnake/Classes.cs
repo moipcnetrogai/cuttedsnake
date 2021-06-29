2b5e1397a92f3a99a262801ac7c3275f2b7a791a
moipcnetrogai <57168273+moipcnetrogai@users.noreply.github.com>
2021-06-29 03:47:36 +0300

    Add files via upload

---
+++ wpfsnake/wpfsnake/Classes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+
+namespace wpfsnake{
+    /*class Connection{
+        static string connect = "Server=tcp:mysnakedata.database.windows.net,1433;Initial Catalog=snake;Persist Security Info=False;User ID=myhanyka;Password=Mihanyka_11;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static SqlConnection MyConnection = new SqlConnection(@connect);
+        public static int On(int x){
+            try { MyConnection.Open(); }
+            catch (System.Data.SqlClient.SqlException) { return x = 1; }
+            return x = 0;
+        }
+        public static void Off(){
+            MyConnection.Close();
+        }
+    }
+    class GetMD{
+        public static string Getmd(string input){
+            using (System.Security.Cryptography.MD5 Getmd = System.Security.Cryptography.MD5.Create()){
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = Getmd.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++){
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }*/
+    public class SnakeHighscore{
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+    }
+    public class Mine{
+        public UIElement UiElement { get; set; }
+        public Point Position { get; set; }
+    }
+    public class SnakePart{
+        public UIElement UiElement { get; set; }
+        public Point Position { get; set; }
+        public bool IsHead { get; set; }
+    }
+}
---
+++ wpfsnake/wpfsnake/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfsnake{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    ///
+    public partial class MainWindow : Window{
+        public static string Playername = "";
+        public MainWindow(){
+            InitializeComponent();
+        }
+        public void butlog_Click(object sender, RoutedEventArgs e){
+            butlog.Cursor = Cursors.Wait;
+            if (login.Text != ""){
+                butlog.Cursor = Cursors.Arrow;
+                btnOk.IsEnabled = true;
+                ettentions.Text = "Hello, it's your name?";
+                UserName.Text = login.Text;
+                bdrWelcomeMessage.Visibility = Visibility.Visible;                   
+            }
+            else {
+                butlog.Cursor = Cursors.Arrow;
+                btnOk.IsEnabled = false;
+                UserName.Text = "";
+                ettentions.Text = "Nickname string is empty!";
+                bdrWelcomeMessage.Visibility = Visibility.Visible;
+            }
+        }
+        private void BtnOK_Click(object sender, RoutedEventArgs e){
+            wpfsnake.MainWindow.Playername = UserName.Text;
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
+            this.Close();
+        }
+        private void BtnNO_Click(object sender, RoutedEventArgs e){
+            bdrWelcomeMessage.Visibility = Visibility.Collapsed;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e){
+            try { this.DragMove();}
+            catch (System.InvalidOperationException) { }
+        }
+        private void BtnClose_Click(object sender, RoutedEventArgs e){
+            this.Close();
+        }
+    }
+}
